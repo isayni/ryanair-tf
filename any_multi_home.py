36579335fb40d6efdb969b69b4e4b759757319a2
isayni <lennysep@protonmail.com>
2025-01-28 22:48:12 +0100

    optimize

--- any_multi_home.py
+++ any_multi_home.py
@@ -8,7 +8,6 @@ find ryanair round trips allowing multiple options for the "home" airport
 '''
 
 API_URL   = "https://services-api.ryanair.com/farfnd/v4/"
-MYPROXIES = False
 
 CURRENCY      = os.getenv("CURRENCY")                 # eg PLN
 DATE_FROM     = os.getenv("DATE_FROM")                # eg 2024-06-01
@@ -16,9 +15,15 @@ DATE_TO       = os.getenv("DATE_TO")                  # eg 2024-06-30
 DAYS_MIN      = int(os.getenv("DAYS_MIN"))            # eg 3
 DAYS_MAX      = int(os.getenv("DAYS_MAX"))            # eg 6
 HOME_AIRPORTS = os.getenv("HOME_AIRPORTS").split(',') # eg KRK,KTW
-DEST_AIRPORTS = os.getenv("DEST_AIRPORTS").split(',') # eg FCO,CPH
+DEST_AIRPORTS = os.getenv("DEST_AIRPORTS", "").split(',') # eg FCO,CPH
 PRICE_MAX     = float(os.getenv("PRICE_MAX"))         # eg 600
-PASSENGERS    = int(os.getenv("PASSENGERS"))          # eg 3
+PASSENGERS    = int(os.getenv("PASSENGERS", 1))       # eg 3
+
+def findAlternativeFares(fare):
+    params = {
+
+    }
+    return findFares(params)
 
 def findFares(params):
     response = requests.get(API_URL + "oneWayFares", params=params).json()
@@ -42,27 +47,29 @@ def createTrip(outFare, returnFare):
     return trip
 
 if __name__ == "__main__":
-    outFares = []
     trips = []
-    for home in HOME_AIRPORTS:
-        params = {
-            "currency": CURRENCY,
-            "departureAirportIataCode": home,
-            "outboundDepartureDateFrom": DATE_FROM,
-            "outboundDepartureDateTo": DATE_TO,
-            "priceValueTo": PRICE_MAX - 50,
-        }
-        outFares += [fare for fare in findFares(params) if fare['outbound']['arrivalAirport']['iataCode'] in DEST_AIRPORTS]
+    params = {
+        "currency": CURRENCY,
+        "departureAirportIataCodes": HOME_AIRPORTS,
+        "outboundDepartureDateFrom": DATE_FROM,
+        "outboundDepartureDateTo": DATE_TO,
+        "priceValueTo": PRICE_MAX - 50,
+    }
+    if "" not in DEST_AIRPORTS:
+        params["arrivalAirportIataCodes"] = DEST_AIRPORTS
+
+    outFares = findFares(params)
 
     for outFare in outFares:
         params = {
             "currency": CURRENCY,
             "departureAirportIataCode": outFare['outbound']['arrivalAirport']['iataCode'],
+            "arrivalAirportIataCodes": HOME_AIRPORTS,
             "outboundDepartureDateFrom": getDate(outFare['outbound']['arrivalDate'], DAYS_MIN),
             "outboundDepartureDateTo": getDate(outFare['outbound']['arrivalDate'], DAYS_MAX),
             "priceValueTo": (PRICE_MAX - outFare['outbound']['price']['value']),
         }
-        returnFares = [fare for fare in findFares(params) if fare['outbound']['arrivalAirport']['iataCode'] in HOME_AIRPORTS]
+        returnFares = findFares(params)
         for returnFare in returnFares:
             trips.append(createTrip(outFare, returnFare))
 
